21a6dc414604a4d5d54522f27c4e59882b71dd76
Italo SF <italosilvafernandesprog@gmail.com>
2023-12-15 00:07:49 -0300

    Fix/ProdutoModel

--- src/models/produto_model.py
+++ src/models/produto_model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String, func
 from sqlalchemy.orm import relationship
-
+from src.models.marca_model import MarcaModel
+from src.models.categoria_model import CategoriaModel
 from src.core.configs import settings
 
 
@@ -17,5 +18,5 @@ class ProdutoModel(settings.DBBaseModel):
     categoria_id = Column(Integer, ForeignKey("categorias.id"), nullable=False)
     unidade_de_venda = Column(String(20), nullable=False)
     valor = Column(Float, nullable=False)
-    marca = relationship("MarcaModel", back_populates="produtos", lazy="joined")
-    categoria = relationship("CategoriaModel", back_populates="produtos", lazy="joined")
+    marca = relationship(MarcaModel, back_populates="produtos", lazy="joined")
+    categoria = relationship(CategoriaModel, back_populates="produtos", lazy="joined")
