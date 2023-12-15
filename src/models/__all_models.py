5d3406a4020f4fd5907ff4cafca17f38ed52d04b
Italo SF <italosilvafernandesprog@gmail.com>
2023-12-14 23:29:37 -0300

    Added __all_models

---
+++ src/models/__all_models.py
@@ -0,0 +1,5 @@
+from src.models.produto_model import ProdutoModel
+from src.models.categoria_model import CategoriaModel
+from src.models.marca_model import MarcaModel
+from src.models.usuario_model import UsuarioModel
+from src.models.permissao_model import PermissaoModel
